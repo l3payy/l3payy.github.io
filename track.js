13612abd67484483a413231d25a967d8d0c096b2
l3payy <ya.lpy@ya.ru>
2026-09-08 23:14:47 +0300

    2

--- track.js
+++ track.js
@@ -1,6 +1,6 @@
 // Данные трека в одном месте.
 // Меняешь здесь — обновляется и на десктопе, и на будущей моб-версии.
 window.TRACK_INFO = {
-  title: 'Mad Noise',
-  artist: 'Outdate Electro'
+  title: 'Angels Dont Try Save Me',
+  artist: 'stvrshine'
 };
