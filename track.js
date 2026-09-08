4c07acd98e7e473f649742dc6873abb0ed564158
l3payy <ya.lpy@ya.ru>
2026-09-08 23:47:29 +0300

    2

--- track.js
+++ track.js
@@ -1,6 +1,6 @@
 // Данные трека в одном месте.
 // Меняешь здесь — обновляется и на десктопе, и на будущей моб-версии.
 window.TRACK_INFO = {
-  title: 'Angels Dont Try Save Me',
-  artist: 'stvrshine'
+  title: 'pussypodium',
+  artist: 'akiaura, LONOWN'
 };
